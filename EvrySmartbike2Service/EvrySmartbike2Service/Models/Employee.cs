cb7150b0b3c5b734a701b05542348986ea58b1ff
Jullix993 <Julius.van.mansvelt@gmail.com>
2018-05-18 22:13:20 +0200

    e

--- EvrySmartbike2Service/EvrySmartbike2Service/Models/Employee.cs
+++ EvrySmartbike2Service/EvrySmartbike2Service/Models/Employee.cs
@@ -12,6 +12,8 @@ namespace EvrySmartbike2Service.Models
         public Guid EmployeeID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
 
     }
 }
